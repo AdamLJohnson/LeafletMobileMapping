1ab3a8db6688ec71477f35bf339b95ef76a532d5
Adam Johnson <columbus@adamlj.com>
2013-06-14 12:34:42 -0700

    Testing navigation

---
+++ LeafletMobileMappingApp/App/viewmodels/test.js
@@ -0,0 +1,15 @@
+﻿define(['durandal/http', 'durandal/app'], function (http, app) {
+
+    var vm = {
+        viewAttached: viewAttached,
+        activate: activate
+    };
+    return vm;
+
+    function activate(context) {
+    }
+
+    function viewAttached(view) {
+
+    }
+});
